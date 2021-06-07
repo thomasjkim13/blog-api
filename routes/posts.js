5604bc3f2c0d56b03a708b11d17ac0f309caec15
Thomas Kim <thomaskim@Thomass-Mac-mini-2.local>
2021-06-06 23:41:58 -0400

    create post update route

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const User = require('../models/User')
+// const User = require('../models/User')
 const Post = require('../models/Post')
 
 // Create Post
@@ -8,6 +8,29 @@ router.post('/', async (req, res) => {
   try {
     const savedPost = await newPost.save()
     res.status(200).json(savedPost)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+// Update post
+router.put('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (post.username === req.body.username) {
+      try {
+        const updatedPost = await Post.findByIdAndUpdate(req.params.id, {
+          $set: req.body
+        },
+        { new: true }
+        )
+        res.status(200).json(updatedPost)
+      } catch (err) {
+        res.status(500).json(err)
+      }
+    } else {
+      res.status(401).json('You can update only your post!')
+    }
   } catch (err) {
     res.status(500).json(err)
   }
